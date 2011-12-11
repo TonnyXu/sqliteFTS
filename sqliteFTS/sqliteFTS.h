63d06fe49e075187026a7d98d25859b4cbca7661
Tonny Xu <tonny.xu@gmail.com>
2011-12-11 15:53:30 +0900

    Initial Commit

---
+++ sqliteFTS/sqliteFTS.h
@@ -0,0 +1,13 @@
+//
+//  sqliteFTS.h
+//  sqliteFTS
+//
+//  Created by Tonny Xu on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sqliteFTS : NSObject
+
+@end
---
+++ sqliteFTS/sqliteFTS.m
@@ -0,0 +1,23 @@
+//
+//  sqliteFTS.m
+//  sqliteFTS
+//
+//  Created by Tonny Xu on 12/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "sqliteFTS.h"
+
+@implementation sqliteFTS
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
